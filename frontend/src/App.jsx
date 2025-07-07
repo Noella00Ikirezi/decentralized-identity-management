dde9424fe097be820703f4212d50783d28c81813
Noella <nikirezi@myges.fr>
2025-07-07 19:24:58 +0200

    yes
    yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Wallet from './pages/Wallet';
 import Profile from './pages/Profile';
 import Documents from './pages/Documents';
 import Apropos from './pages/Apropos';
-=======
 import './App.css';
 import AppRoutes from './components/AppRoutes';
->>>>>>> 033a0c7cf4583b98f5ecba33ed7edf0aa16cd4bf
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,20 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-<<<<<<< HEAD
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-=======
       <BrowserRouter>
->>>>>>> 033a0c7cf4583b98f5ecba33ed7edf0aa16cd4bf
         <App />
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
-
